45e8b9340e79e67b61b21180802149407591998e
KSH <sooooooohyeon5@naver.com>
2022-01-21 17:53:40 +0900

    auth 데이터만 수정

--- src/tweets/auth.js
+++ src/tweets/auth.js
@@ -11,13 +11,13 @@ let user = [
         password: '111pass'
     },
     {
-        id: 1,
-        email: '111@gmail.com',
-        password: '111pass'
+        id: 2,
+        email: '222@gmail.com',
+        password: '222pass'
     },
     {
-        id: 1,
-        email: '111@gmail.com',
-        password: '111pass'
+        id: 3,
+        email: '333@gmail.com',
+        password: '333pass'
     },
 ]
