bc8b563f2b93b3ead786730c64d40f0fbd47c1a4
김 소 현 <sooooooohyeon5@naver.com>
2022-01-22 15:55:45 +0900

    로그인, 회원가입 추가

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import posts from "./tweets/posts";
+import express from 'express'
+import users from './tweets/auth'
+import posts from './tweets/posts'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.json());
+app.use(express.json())
 
-app.use('/posts', posts);
+app.use('/posts', posts)
+app.use('/auth', users)
 
 app.listen(port, () => {
-  console.log(`짜자잔~ >>>> http://localhost:${port}`);
-});
+  console.log(`짜자잔~ >>>> http://localhost:${port}`)
+})
--- src/tweets/auth.js
+++ src/tweets/auth.js
@@ -1,23 +1,64 @@
-import { Router } from "express";
+import { Router } from 'express'
 
-const route = Router();
+const auth = Router()
 
-let nextId = 4;
+let nextId = 4
 
-let user = [
-    {
-        id: 1,
-        email: '111@gmail.com',
-        password: '111pass'
-    },
-    {
-        id: 2,
-        email: '222@gmail.com',
-        password: '222pass'
-    },
-    {
-        id: 3,
-        email: '333@gmail.com',
-        password: '333pass'
-    },
+let users = [
+  {
+    id: 1,
+    email: '111@gmail.com',
+    password: '111pass',
+  },
+  {
+    id: 2,
+    email: '222@gmail.com',
+    password: '222pass',
+  },
+  {
+    id: 3,
+    email: '333@gmail.com',
+    password: '333pass',
+  },
 ]
+
+auth.get('/', (req, res) => {
+  res.json(users) //users 형식의 응답을 보낸다.
+}) //전체 회원 조회
+
+auth.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const Emailcheck = users.findIndex((user) => user.email === +req.params.email)
+  const Passwordcheck = users.findIndex(
+    (user) => user.password === +req.params.password,
+  )
+
+  if (Emailcheck && Passwordcheck) {
+    res.json(users.filter((user) => user.id === +req.params.id)[0])
+  } else {
+    return res.json({
+      error: '아이디, 패스워드가 일치하지 않습니다.',
+    })
+  }
+}) //일단 아이디, 패스워드 치면 id값 나오게끔
+
+auth.post('/register', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (users.findIndex((post) => users.email === +req.params.email)) {
+    return res.json({
+      error: '이미 존재하는 이메일입니다.',
+    })
+  }
+
+  users.push({
+    id: nextId++,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  res.json(users)
+})
+
+export default auth
